e28380dd1833d263b7dccb24fe91b8037d15d9e0
MaikenDANGELO <maiken.dangelo@gmail.com>
2025-06-12 21:50:33 +0200

    first deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   ssr: false,
+  nitro: {
+    preset: 'static'
+  },
+  app: {
+    baseURL: '/Portfolio-Nuxt/'
+  },
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
